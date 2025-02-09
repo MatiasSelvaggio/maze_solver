62ba4c43424c95649c347870623aa9260f675eb7
Matias D. Selvaggio <matias6selvaggio@gmail.com>
2025-02-09 20:36:51 +0000

    draw betewn 2 cell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from window import Window
 from geometry import *
+from cell import Cell
 
 def main():
     window = Window(800, 600)
@@ -19,6 +20,30 @@ def main():
     window.draw_line(line_b, "black")
     window.draw_line(line_c, "blue")
     window.draw_line(line_d, "green")
+
+    c1 = Cell(window)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(window)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(window)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(window)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     window.wait_for_close()
 
